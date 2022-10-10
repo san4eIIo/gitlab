6029ec7dc38d593cf7db4c31a1a3405e3d15fe95
San4eIIo <naumchuk18122002@gmail.com>
2022-10-10 23:35:31 +0300

    'redacted'

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,18 @@
 #include "calculator.h"
-
+//adding numbers
+//=)
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
-
+//=)
+//substracting numbers
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
-
+//=)
+//Multiplication of numbers
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
